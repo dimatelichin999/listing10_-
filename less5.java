249634e413f0151e9eb801d19e2051e53b0c0133
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-03-31 18:37:05 +0300

    порівняння

---
+++ less5.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class less5 {
+
+    public static void main(String args[]){
+
+        int [] x1 = {1,2,3,4};
+        int [] x2 = {1,2,3,4};
+
+        Arrays.equals(x1,x2);
+
+        System.out.println(Arrays.equals(x1, x2));
+    }
+}

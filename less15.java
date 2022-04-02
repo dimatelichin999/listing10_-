83cf136992b42a2ed089fd6f752085b1e7776b0f
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-02 18:31:21 +0300

    текст

---
+++ less15.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class less15 {
+
+    public static void main(String args[]){
+
+        String[] x1 ={"java","phyton"};
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(x1));
+
+        System.out.println(list);
+    }
+}
